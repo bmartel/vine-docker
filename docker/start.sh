5ce5d4c59041b7cccd7d3accb77508eddbc60306
bmartel <brandonmartel@gmail.com>
2018-10-27 18:59:17 -0500

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -4,8 +4,6 @@ if [ "$RAILS_ENV" == 'production' ]; then
   bundle exec rails assets:precompile
   rm -rf node_modules
 else
-  bundle exec rake db:create
-  bundle exec rake db:migrate
   bin/webpack-dev-server &
 fi
 
